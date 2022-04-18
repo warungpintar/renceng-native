b8ffda73bedc841d53e001f94d324bc29994f0e1
Muhammad Irsyad <irsyart@gmail.com>
2022-04-18 20:40:26 +0700

    feat: add view stories

--- example/components/View/View.stories.tsx
+++ example/components/View/View.stories.tsx
@@ -1,7 +1,5 @@
-// stories/MyButton.stories.tsx
 import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
-import {Text} from 'react-native';
 import {View} from 'renceng-native';
 
 export default {
@@ -9,15 +7,53 @@ export default {
   component: View,
 } as ComponentMeta<typeof View>;
 
-export const Basic: ComponentStory<typeof View> = args => (
-  <View {...args}>
-    <Text>Hmm</Text>
-  </View>
-);
+export const Basic: ComponentStory<typeof View> = args => <View {...args} />;
+
+export const Rounded: ComponentStory<typeof View> = args => <View {...args} />;
+
+export const Shadow: ComponentStory<typeof View> = args => <View {...args} />;
+
+export const Color: ComponentStory<typeof View> = args => <View {...args} />;
 
 Basic.args = {
-  borderColor: 'red',
-  borderRadius: 4,
-  borderWidth: 2,
-  backgroundColor: 'yellow',
+  width: 200,
+  height: 100,
+  bg: 'white',
+  borderWidth: 1,
+  borderColor: 'black',
+  borderStyle: 'solid',
+};
+
+Rounded.args = {
+  width: 200,
+  height: 100,
+  bg: 'white',
+  borderWidth: 1,
+  borderColor: 'black',
+  borderStyle: 'solid',
+  borderRadius: 8,
+};
+
+Shadow.args = {
+  width: 200,
+  height: 100,
+  bg: 'white',
+  shadowColor: 'black',
+  shadowOffset: {
+    width: 4,
+    height: 8,
+  },
+  shadowOpacity: 0.5,
+  shadowRadius: 8,
+
+  elevation: 8,
+};
+
+Color.args = {
+  width: 200,
+  height: 100,
+  bg: '#FECF28',
+  borderWidth: 1,
+  borderColor: 'black',
+  borderStyle: 'solid',
 };
