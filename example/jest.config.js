b288f2961847b62391f255898b88bbbb66542a10
Muhammad Irsyad <irsyart@gmail.com>
2022-04-19 16:02:28 +0700

    feat: add more unit test for view component

--- example/jest.config.js
+++ example/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'react-native',
   moduleNameMapper: {
-    "^react($|/.+)": "<rootDir>/node_modules/react$1", // makes sure all React imports are running off of the one in this package.
-  }
-}
+    '^react($|/.+)': '<rootDir>/node_modules/react$1', // makes sure all React imports are running off of the one in this package.
+  },
+};
