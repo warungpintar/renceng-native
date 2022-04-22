7e3fa3463cbaee5f92265cc87ace37b523abe1a8
Muhammad Irsyad <irsyart@gmail.com>
2022-04-22 15:29:19 +0700

    chore: remove unused wrapper component

--- example/components/Showcase/index.ts
+++ example/components/Showcase/index.ts
@@ -1,6 +1,5 @@
 import ShowcaseSection from './ShowcaseSection';
 import ShowcaseItem from './ShowcaseItem';
 import Showcase from './Showcase';
-import Subtitle from './Subtitle';
 
-export {ShowcaseSection, ShowcaseItem, Showcase, Subtitle};
+export {ShowcaseSection, ShowcaseItem, Showcase};
